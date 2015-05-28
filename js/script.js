b8b25dc4f2c04c8742f87b56debd0ee6deb49c47
Naomi <naomiventura@live.com>
2015-05-28 11:05:55 +0200

    ajout contacts

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+contacts = [{
+    prenom: "Julien",
+    nom: "Grillot"
+}, {
+    prenom: "John",
+    nom: "Smith"
+}, {
+    prenom : "Yoan",
+    nom: "Dieu"
+}, {
+    prenom:"Erwan" ,
+    nom: "Kezzar"
+} , {
+    prenom: "Naomi"  ,
+    nom: "Ventura"
+},  {
+    prenom: "Jean" ,
+    nom: "Dupond"
+}];
+
+
